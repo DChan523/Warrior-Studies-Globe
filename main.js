66292dafb7e4f4ec2d42a660a5c01c23aec7b75f
Dylan <dchan2614@gmail.com>
2022-04-13 16:59:50 -0400

    added comments

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ let clock = new THREE.Clock();  //creates a new clock object used to time stuff
 const scene = new THREE.Scene();    //makes a new scene
 const camera = new THREE.PerspectiveCamera( 75, innerWidth / innerHeight, 0.1, 1000 ); //perspective on the object
 
-const renderer = new THREE.WebGLRenderer(); //??
+const renderer = new THREE.WebGLRenderer(
+    {
+        antialias:true  //gets rid of some of the jaggedness
+    }
+); 
 renderer.setSize(innerWidth, innerHeight );
+renderer.setPixelRatio(window.devicePixelRatio);// the internet said this was good
 document.body.appendChild( renderer.domElement ); // adds object to page when called
 
 time = clock.getElapsedTime();  //total time elapsed in ms since the clock was created
@@ -18,15 +23,22 @@ const sphere = new THREE.Mesh(
     
     scene.add( sphere );//adds my sphere object to the scene
 
-    camera.position.z = 15;
+    camera.position.z = 15; //must be greater than the radius of the object
 
     function animate() {
-        requestAnimationFrame( animate );
-
+        requestAnimationFrame( animate );//creates a loop that will draw the scene everytime the screen is refreshed
+        
+        //this runs every frame assuming 60 fps
         sphere.rotation.x += delta * 1; // this controls the vertical spin for some reason
         sphere.rotation.y += 0.005; //this controls the horizontal spin
 
         renderer.render( scene, camera );
     };
 
+    //this will hopefully let me click on individual continents to get a modal pop up might need to do math to do it tho
+    function selectCont(){
+
+    }
+
         animate();
+    
