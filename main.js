a114d0a160c3023b8976116de0c322e796fdb5d2
Dylan <dchan2614@gmail.com>
2022-04-13 12:48:50 -0400

    crated a sphere

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
+//import * as THREE from 'three'   get some error when i import the module here 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize(window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.SphereGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const sphere = new THREE.Mesh( geometry, material );
+const sphere = new THREE.Mesh( 
+    new THREE.SphereGeometry(5,50,50), 
+    new THREE.MeshBasicMaterial({
+        color: 0xFF5733
+    }));
+    
     scene.add( sphere );
 
-    camera.position.z = 5;
+    camera.position.z = 15;
 
     function animate() {
         requestAnimationFrame( animate );
